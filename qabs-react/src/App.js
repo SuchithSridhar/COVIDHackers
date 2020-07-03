34b45012dd85eb675314960c4d6d67d0292a4003
Suchith Sridhar <suchithsridhar@gmail.com>
2020-07-04 02:48:34 +0300

    Home Page

--- qabs-react/src/App.js
+++ qabs-react/src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
-
 import "./App.css";
+import Home from "./pages/home";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 import LoginForm from "./components/loginForm";
 
 function App() {
   return (
     <div className="App">
-      <LoginForm />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login" component={LoginForm} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ qabs-react/src/pages/home.jsx
@@ -0,0 +1,84 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Home extends Component {
+  state = {
+    ministries: [{ name: "Something" }, { name: "Different" }],
+    currentMinistry: "",
+    currentBranch: "",
+    currentService: "",
+  };
+
+  getMinistries = () => {
+    const ministries = this.state.ministries;
+    let names = [];
+    for (let index in ministries)
+      names.push({ id: index, name: ministries[index].name });
+
+    return names;
+  };
+
+  getBranches = () => {
+    const ministry = this.state.currentMinistry;
+    let branches = this.state.ministries[ministry];
+    if (ministry === "Something") branches = ["fahaheel", "Mangaf"];
+    else branches = ["abu halifa"];
+    return branches;
+  };
+
+  handleMinClick = (ministry) => {
+    let curState = { ...this.setState };
+    curState.currentMinistry = ministry;
+    this.setState(curState);
+  };
+
+  handleBraClick = (branch) => {
+    let curState = { ...this.setState };
+    curState.currentBranch = branch;
+    this.setState(curState);
+  };
+
+  render() {
+    return (
+      <div>
+        <div>
+          <label>Ministry Services</label>
+          <Link to="/login"></Link>
+        </div>
+        <div>
+          <h2>Ministry</h2>
+          {this.getMinistries().map((item) => (
+            <li key={item.id} id={item.name}>
+              <button
+                onClick={() => {
+                  this.handleMinClick(item.name);
+                }}
+              >
+                {item.name}
+              </button>
+            </li>
+          ))}
+
+          {this.state.currentMinistry && (
+            <div>
+              <h2>Branches</h2>
+              {this.getBranches().map((item) => (
+                <li key={item.id} id={item.name}>
+                  <button
+                    onClick={() => {
+                      this.handleBraClick(item.name);
+                    }}
+                  >
+                    {item.name}
+                  </button>
+                </li>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
