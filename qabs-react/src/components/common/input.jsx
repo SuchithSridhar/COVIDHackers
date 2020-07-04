82b1b23c6981f780c0e37ab1a5c2c4c9554f5926
Suchith Sridhar <suchithsridhar@gmail.com>
2020-07-04 11:42:26 +0300

    -

--- qabs-react/src/components/common/input.jsx
+++ qabs-react/src/components/common/input.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import '../css/loginForm.css'
+import "../css/loginForm.css";
 
 const Input = ({ name, label, error, ...rest }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}></label>
-      // <div className='textbox'>
-      <input
-        {...rest}
-        name={name}
-        id={name}
-        placeHolder={name}
-        className="form-control"
-      // />
-    </div>
+      <div className="textbox">
+        <input
+          {...rest}
+          name={name}
+          id={name}
+          placeHolder={name}
+          className="form-control"
+        ></input>
+      </div>
     </div>
   );
 };
